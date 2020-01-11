99bed47613e5ce6940f02119f9aabf7d5ea34075
JasanHdz <jasan_azael@hotmail.es>
2020-01-10 21:14:27 -0600

    dividing problems into functions

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-let $area_de_dibujo = document.getElementById('area_de_dibujo')
-let lienzo = $area_de_dibujo.getContext("2d");
-console.log(lienzo);
-
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100, 100);
-lienzo.lineTo(200, 200); // trazar una linea
-lienzo.moveTo(100, 100);
-lienzo.lineTo(200, 100);
-lienzo.moveTo(200, 100);
-lienzo.lineTo(200, 200);
-lienzo.stroke();
-lienzo.closePath(); // cerrar lienzo o terminar trazo
-
-function drawLine(color, xinicial, yincial, xfinal, yfinal) {
-
-}
-
 class Tokens {
   constructor() {
-    this.tokens = document.getElementById('tokens').value;
+    this.debuggerContainer = document.getElementById('debugger');
+    this.canvas = document.getElementById('area_de_dibujo');
+    this.lienzo = this.canvas.getContext('2d');
+    this.text = document.getElementById('tokens').value;
+    this.arrayMessages = [];
     this.reservTokens = {
       Programa: "Programa",
       Inicio: "Inicio",
@@ -38,13 +23,79 @@ class Tokens {
     };
   }
 
-  getText() {
-    console.log(this.tokens);
-    console.log(this.reservTokens)
+  addTemplate(message) {
+    this.debuggerContainer.innerHTML = '';
+    if (!this.arrayMessages.includes(message)) {
+      this.arrayMessages.push(message);
+      console.log(this.arrayMessages);
+      this.arrayMessages.map(msg => {
+        const template = document.createElement('p');
+        template.textContent = msg;
+        return this.debuggerContainer.appendChild(template);
+      });
+    }
   }
 
+  getText() {
+    this.tokens = this.text.split(" ");
+    const arrayKeyWords = [];
+    this.tokens.forEach(keyword => {
+      if (!arrayKeyWords.includes(keyword)) {
+        arrayKeyWords.push(keyword);
+        return this.reservedWords(keyword);
+      } else {
+        return this.addTemplate("Las palabras no pueden repetirse");
+      }
+    });
+    console.log(this.tokens.indexOf(this.reservTokens.Programa));
+    console.log(this.text);
+    this.addTemplate(this.text);
+    this.addTemplate("Palabras reservadas: ");
+    this.addTemplate(JSON.stringify(this.reservTokens));
+    console.log(this.reservTokens)
+    this.drawLine();
+  }
+
+  reservedWords(word) {
+    switch (word) {
+      case this.reservTokens.Programa: {
+        alert("La palabra Programa está contendia");
+      }
+      case this.reservTokens.Inicio: {
+        
+      } 
+      case this.reservTokens.Fin: {}
+      case this.reservTokens.DibujarCirculo: {} 
+      case this.reservTokens.DibujarRectangulo: {}
+      case this.reservTokens.DibujarTriangulo: {}
+      case this.reservTokens.EliminarFigura: {}
+      case this.reservTokens.Dormir: {}
+      case this.reservTokens.ROJO: {}
+      case this.reservTokens.AZUL: {}
+      case this.reservTokens.AMARILLO: {}
+      case this.reservTokens.Constante: { }
+      default: console.log("Sin palabras reservadas :(");
+    }
+  }
+
+  drawLine() {
+    this.lienzo.beginPath();
+    this.lienzo.strokeStyle = "red";
+    this.lienzo.moveTo(100, 100);
+    this.lienzo.lineTo(200, 200); // trazar una linea
+    this.lienzo.moveTo(100, 100);
+    this.lienzo.lineTo(200, 100);
+    this.lienzo.moveTo(200, 100);
+    this.lienzo.lineTo(200, 200);
+    this.lienzo.stroke();
+    this.lienzo.closePath(); // cerrar lienzo o terminar trazo
+  }
+
+
   dibujarCirculo(xinicial, yinicial, xfinal, yfinal, id) {}
-  dibujarRectangulo(xinicial, yinicial, xfinal, yfinal, id) {}
+  dibujarRectangulo(xinicial, yinicial, xfinal, yfinal, id) {
+   
+  }
   dibujarTriangulo(xinicial, yinicial, xfinal, yfinal, id) { }
   eliminarFigura(id) { }
   dormir(time) {}
